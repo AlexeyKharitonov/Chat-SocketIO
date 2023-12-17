d2cba685a93cbe63d70f7044ec768261bf90caff
Alexey Kharitonov <alexeywest024@list.ru>
2023-12-17 13:39:02 +0300

    Remove build from tracking

--- server/build/route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-const router = Router();
-router.get("/", (req, res) => {
-    res.send("Проверка связи!!!!!!!!!!!1");
-});
-export default router;
-//# sourceMappingURL=route.js.map
